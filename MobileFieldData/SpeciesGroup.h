4668ae30d240a8850848c66101a34b67f331ac7c
chrisala <chris.godwin.ala@gmail.com>
2013-02-20 06:12:18 +0000

    Added species groups to the model (although they are not yet used)
    git-svn-id: http://ala-mobile.googlecode.com/svn/trunk/fielddata-mobile@307 6a6df900-09af-505f-f2bd-838eb8a94856

---
+++ MobileFieldData/SpeciesGroup.h
@@ -0,0 +1,35 @@
+//
+//  SpeciesGroup.h
+//  MobileFieldData
+//
+//  Created by Chris Godwin on 20/02/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Species, SpeciesGroup;
+
+@interface SpeciesGroup : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString* groupDescription;
+@property (nonatomic, retain) SpeciesGroup *parent;
+@property (nonatomic, retain) NSSet *species;
+@property (nonatomic, retain) NSSet *subgroups;
+@end
+
+@interface SpeciesGroup (CoreDataGeneratedAccessors)
+
+- (void)addSpeciesObject:(Species *)value;
+- (void)removeSpeciesObject:(Species *)value;
+- (void)addSpecies:(NSSet *)values;
+- (void)removeSpecies:(NSSet *)values;
+
+- (void)addSubgroupsObject:(SpeciesGroup *)value;
+- (void)removeSubgroupsObject:(SpeciesGroup *)value;
+- (void)addSubgroups:(NSSet *)values;
+- (void)removeSubgroups:(NSSet *)values;
+
+@end
---
+++ MobileFieldData/SpeciesGroup.m
@@ -0,0 +1,22 @@
+//
+//  SpeciesGroup.m
+//  MobileFieldData
+//
+//  Created by Chris Godwin on 20/02/13.
+//
+//
+
+#import "SpeciesGroup.h"
+#import "Species.h"
+#import "SpeciesGroup.h"
+
+
+@implementation SpeciesGroup
+
+@dynamic name;
+@dynamic groupDescription;
+@dynamic parent;
+@dynamic species;
+@dynamic subgroups;
+
+@end
